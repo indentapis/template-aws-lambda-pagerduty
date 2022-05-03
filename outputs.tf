380e63c7c360cdadfe64223de6b422b0931ab8a8
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-05-03 10:04:02 -0400

    fix(terraform): module name

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "pagerduty_auto_approval_webhook_url" {
-  value       = module.pagerduty_auto_approval_webhook.api_base_url
+  value       = module.pagerduty-auto-approval-webhook.api_base_url
   description = "The URL of the deployed Lambda"
 }
